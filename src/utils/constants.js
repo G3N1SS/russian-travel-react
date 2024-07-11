6aadc3022923e49b9e7d3cfe0fa3b3ca412e5a74
unknown <areg.sarkisyan.99@mail.ru>
2024-07-11 19:34:06 +0300

    image fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,69 +1,69 @@
-import __item1 from '../images/__item1.jpg'
-import __item2 from '../images/__item2.jpg'
-import __item3 from '../images/__item3.jpg'
-import __item4 from '../images/__item4.jpg'
-import __item5 from '../images/__item5.jpg'
-import __item6 from '../images/__item6.jpg'
-import __item7 from '../images/__item7.jpg'
-import __item8 from '../images/__item8.jpg'
-import __item9 from '../images/__item9.jpg'
-import __item10 from '../images/__item10.jpg'
-import __item11 from '../images/__item11.jpg'
-import __item12 from '../images/__item12.jpg'
-import img1 from '../images/__image1.jpg'
-import img2 from '../images/__image2.jpg'
-import img3 from '../images/__image3.jpg'
-import img4 from '../images/__image4.jpg'
-import img5 from '../images/__image5.jpg'
+import item1 from '../images/item1.jpg'
+import item2 from '../images/item2.jpg'
+import item3 from '../images/item3.jpg'
+import item4 from '../images/item4.jpg'
+import item5 from '../images/item5.jpg'
+import item6 from '../images/item6.jpg'
+import item7 from '../images/item7.jpg'
+import item8 from '../images/item8.jpg'
+import item9 from '../images/item9.jpg'
+import item10 from '../images/item10.jpg'
+import item11 from '../images/item11.jpg'
+import item12 from '../images/item12.jpg'
+import img1 from '../images/image1.jpg'
+import img2 from '../images/image2.jpg'
+import img3 from '../images/image3.jpg'
+import img4 from '../images/image4.jpg'
+import img5 from '../images/image5.jpg'
 
 const photogrid = [
   {
     name: 'Вид из поезда',
-    link: __item1
+    link: item1
   },
   {
     name: 'Вершина',
-    link: __item2
+    link: item2
   },
   {
     name: 'Поле',
-    link: __item3
+    link: item3
   },
   {
     name: 'Леса',
-    link: __item4
+    link: item4
   },
   {
     name: 'Лед',
-    link: __item5
+    link: item5
   },
   {
     name: 'Пляж',
-    link: __item6
+    link: item6
   },
   {
     name: 'Горы',
-    link: __item7
+    link: item7
   },
   {
     name: 'Знак',
-    link: __item8
+    link: item8
   },
   {
     name: 'Туман',
-    link: __item9
+    link: item9
   },
   {
     name: 'Пейзаж',
-    link: __item10
+    link: item10
   },
   {
     name: 'Обрыв',
-    link: __item11
+    link: item11
   },
   {
     name: 'Облака',
-    link: __item12
+    link: item12
   }
 ]
 
